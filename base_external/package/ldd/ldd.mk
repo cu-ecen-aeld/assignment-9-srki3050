16550ab08e822b3bada3e488b9027a80d8158177
Sricharan Kidambi <srki3050@colorado.edu>
2022-10-30 01:12:11 +0000

    Full Test Passed for Assignment-7

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -10,9 +10,9 @@ LDD_VERSION = '58bcf72947bc7da1148d6dcae2a7b05e53ad14fd'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-LDD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-srki3050.git'
-LDD_ASSIGNMENTS_SITE_METHOD = git
-LDD_ASSIGNMENTS_GIT_SUBMODULES = YES
+LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-srki3050.git'
+LDD_SITE_METHOD = git
+LDD_GIT_SUBMODULES = YES
 
 #As described in the Assignment instructuion - using Module Subdirs option to build the misc and scull modules
 LDD_MODULE_SUBDIRS = misc_modules
@@ -20,7 +20,7 @@ LDD_MODULE_SUBDIRS += scull
 
 
 #  Add install statements to install the misc and scull modules
-define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
+define LDD_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
