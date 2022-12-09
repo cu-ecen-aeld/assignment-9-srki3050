39bafebc453e285ca33c3e666f5f2a0803a9df44
srki3050 <srki3050@colorado.edu>
2022-12-09 18:34:02 +0000

    Added Hello Modules

--- base_external/rootfs_overlay/etc/init.d/S98lddmodules.sh
+++ base_external/rootfs_overlay/etc/init.d/S98lddmodules.sh
@@ -2,12 +2,12 @@
 
 case "$1" in
     start)
-        modprobe hello
-        /usr/bin/module_load faulty
+	modprobe hello
+     	/usr/bin/module_load faulty
         /usr/bin/scull_load
         ;;
     stop)
-        rmmod hello
+	rmmod hello
         /usr/bin/module_unload faulty
         /usr/bin/scull_unload
         ;;
