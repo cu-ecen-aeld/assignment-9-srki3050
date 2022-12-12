53999eae7a5581ad348c2f729c6d1b8395153967
srki3050 <srki3050@colorado.edu>
2022-12-12 23:41:25 +0000

    Full Test passed locally

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,13 +6,15 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '173f4ffede3cba81f8abd8369bbe58c721cc1f2c'
+AESD_ASSIGNMENTS_VERSION = 'f04c468f7fb9afa0f565b951304b597fe8c03d04'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
 AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-srki3050.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+
+AESD_ASSIGNMENTS_MODULE_SUBDIRS += aesd-char-driver/
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
@@ -31,8 +33,9 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop.sh $(TARGET_DIR)/etc/init.d/S99aesdsocket
 # for assignment 8
-        $(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin	
+        $(INSTALL) -m 0755 $(@D)/aesd-char-driver/* $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment8/* $(TARGET_DIR)/usr/bin	
 endef
 
+$(eval $(kernel-module))
 $(eval $(generic-package))
--- base_external/rootfs_overlay/etc/init.d/S98lddmodules.sh
+++ base_external/rootfs_overlay/etc/init.d/S98lddmodules.sh
@@ -2,16 +2,19 @@
 
 case "$1" in
     start)
-	modprobe hello
      	/usr/bin/module_load faulty
         /usr/bin/scull_load
+	/usr/bin/aesdchar_load aesdchar
+	modprobe hello
         ;;
     stop)
-	rmmod hello
         /usr/bin/module_unload faulty
         /usr/bin/scull_unload
+	/usr/bin/aesdchar_unload aesdchar
+	rmmod hello
         ;;
     *)
+	echo "Usage: $0 {start|stop}"
     exit 1
 esac
 
